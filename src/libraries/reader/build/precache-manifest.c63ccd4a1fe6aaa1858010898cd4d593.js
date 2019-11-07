10ab2aed050fd63976c7f78832a138d3f1e9febc
Lawson Cheng <inbox@lawsoncheng.com>
2019-11-07 15:25:18 +0800

    Build for prod.

--- src/libraries/reader/build/precache-manifest.c63ccd4a1fe6aaa1858010898cd4d593.js
+++ src/libraries/reader/build/precache-manifest.c63ccd4a1fe6aaa1858010898cd4d593.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2742fa61ac2b0ebde30b09d409e33b9b",
+    "revision": "95979178cbbc7846e4a739cd17c96343",
     "url": "/index.html"
   },
   {
-    "revision": "79221f2d2fe7eb142a86",
-    "url": "/static/css/main.3a4079ba.chunk.css"
+    "revision": "7b684bc01877af94998d",
+    "url": "/static/css/main.ee3c0581.chunk.css"
   },
   {
     "revision": "80d7f1a280438f74ea0c",
     "url": "/static/js/2.88f299f7.chunk.js"
   },
   {
-    "revision": "79221f2d2fe7eb142a86",
+    "revision": "7b684bc01877af94998d",
     "url": "/static/js/main.ed575e54.chunk.js"
   },
   {
